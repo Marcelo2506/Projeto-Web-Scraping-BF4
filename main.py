d760494e9a2538b24e8b6686b7a79c10a5b4eedb
Marcelo Ricardo <marcello.8@hotmail.com>
2024-11-30 01:25:16 -0300

    Main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+
+link = "https://www.google.com/search?q=cota%C3%A7%C3%A3o+dolar"
+requisicao = requests.get(link)
+print(requisicao)
+
+site = BeautifulSoup(requisicao.text, "html.parser")  # Organiza o código
+print(site.prettify())
+
+# Link do projeto https://www.youtube.com/watch?v=WzUPnQwSQyk
+# Pausado em 16:43
